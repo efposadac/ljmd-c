d088649ab87eea4861498aa85bfe5737afe45056
eposada <eposada@hp83-inf-7.ictp.it>
2014-03-17 14:52:48 +0100

    Add a test for git manage

--- src/ljmd.c
+++ src/ljmd.c
@@ -16,6 +16,8 @@
 /* generic file- or pathname buffer length */
 #define BLEN 200
 
+/* this is a test */
+
 /* a few physical constants */
 const double kboltz=0.0019872067;     /* boltzman constant in kcal/mol/K */
 const double mvsq2e=2390.05736153349; /* m*v^2 in kcal/mol */
